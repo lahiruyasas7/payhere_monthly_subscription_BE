8efa468f675183e69d861eb7c80d7154f2af9815
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-01-03 13:06:26 +0530

    created subscription module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SubscriptionModule } from './subscription/subscription.module';
 
 @Module({
-  imports: [],
+  imports: [SubscriptionModule],
   controllers: [AppController],
   providers: [AppService],
 })
