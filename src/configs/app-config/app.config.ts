dd88394eb4fe6ac97159d86633eb2fbc1c2829e4
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-01-03 13:25:59 +0530

    configured payhere

--- src/configs/app-config/app.config.ts
+++ src/configs/app-config/app.config.ts
@@ -6,4 +6,17 @@ export default registerAs('app', () => ({
   database: {
     databaseUrl: applicationConfig.database.databaseUrl,
   },
+  // Payhere Config
+  payhere: {
+    merchantId: applicationConfig.payhere.merchantId,
+    merchantSecret: applicationConfig.payhere.merchantSecret,
+    chargeUrl: applicationConfig.payhere.chargeUrl,
+    returnUrl: applicationConfig.payhere.returnUrl,
+    cancelUrl: applicationConfig.payhere.cancelUrl,
+    notifyUrl: applicationConfig.payhere.notifyUrl,
+    currency: applicationConfig.payhere.currency,
+    country: applicationConfig.payhere.country,
+    authorizationCode: applicationConfig.payhere.authorizationCode,
+    authUrl: applicationConfig.payhere.authUrl,
+  },
 }));
--- src/configs/app-config/config.ts
+++ src/configs/app-config/config.ts
@@ -3,5 +3,17 @@ require("dotenv").config();
 export const applicationConfig = {
     database: {
         databaseUrl: process.env[`DATABASE_URL`],
-    }
+    },
+    payhere: {
+        merchantId: process.env[`PAYHERE_MERCHANT_ID`],
+        merchantSecret: process.env[`PAYHERE_MERCHANT_SECRET`],
+        chargeUrl: process.env[`PAYHERE_CHARGE_URL`],
+        returnUrl: process.env[`PAYHERE_RETURN_URL`],
+        cancelUrl: process.env[`PAYHERE_CANCEL_URL`],
+        notifyUrl: process.env[`PAYHERE_NOTIFY_URL`],
+        currency: process.env[`PAYHERE_CURRENCY`],
+        country: process.env[`PAYHERE_COUNTRY`],
+        authorizationCode: process.env[`PAYHERE_AUTHORIZATION_CODE`],
+        authUrl: process.env[`PAYHERE_AUTH_URL`],
+      },
 }
