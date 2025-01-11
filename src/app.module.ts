55fafac2b95c70fe5b6284c3a545490cdb604657
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-01-11 21:27:06 +0530

    added config modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SubscriptionModule } from './subscription/subscription.module';
+import { AppConfigModule } from './configs/app-config/app.config.module';
+import { DatabaseConfig } from './configs/database-configs/databse-config.module';
+import { PrismaService } from './configs/database-configs/prisma.service';
+import { PrismaModule } from './configs/database-configs/prisma.module';
 
 @Module({
-  imports: [SubscriptionModule],
+  imports: [AppConfigModule, DatabaseConfig, SubscriptionModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
