836eaa0c020c38f7f3395d740c68b547796cb89f
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-01-11 21:31:12 +0530

    added schedule module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,11 @@ import { AppService } from './app.service';
 import { SubscriptionModule } from './subscription/subscription.module';
 import { AppConfigModule } from './configs/app-config/app.config.module';
 import { DatabaseConfig } from './configs/database-configs/databse-config.module';
-import { PrismaService } from './configs/database-configs/prisma.service';
 import { PrismaModule } from './configs/database-configs/prisma.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [AppConfigModule, DatabaseConfig, SubscriptionModule, PrismaModule],
+  imports: [AppConfigModule, DatabaseConfig, SubscriptionModule, PrismaModule, ScheduleModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
