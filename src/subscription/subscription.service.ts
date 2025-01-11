b3e52aca86d7d2821f886fc87efba143fb30d2b8
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-01-11 21:28:31 +0530

    created service for payment eligibility true for 3 month old user accounts

--- src/subscription/subscription.service.ts
+++ src/subscription/subscription.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from 'src/configs/database-configs/prisma.service';
 
 @Injectable()
-export class SubscriptionService {}
+export class SubscriptionService {
+private readonly logger = new Logger(SubscriptionService.name);
+  constructor(private readonly prisma: PrismaService) {}
+  /**
+   * This method updates the payment_eligibility for tenants
+   * that are 3 months old.
+   */
+  async updatePaymentEligibility(): Promise<void> {
+    const today = new Date();
+    const threeMonthsBoundary = new Date(
+      today.getFullYear(),
+      today.getMonth() - 3,
+      28,
+    );
+
+    try {
+      const updatedTenants = await this.prisma.user.updateMany({
+        where: {
+          created_at: { lte: threeMonthsBoundary },
+          payment_eligibility: false,
+        },
+        data: {
+          payment_eligibility: true,
+        },
+      });
+
+      this.logger.log(
+        `Payment eligibility updated for ${updatedTenants.count} tenants.`,
+      );
+
+      // Retrieve all tenants with payment_eligibility = true
+      const eligibleTenants = await this.prisma.user.findMany({
+        where: {
+          payment_eligibility: true,
+          OR: [
+            { subscription: { status: { not: 'ACTIVE' } } },
+            { subscription: null }, // Include tenants without a subscription
+          ],
+        },
+        select: {
+          email: true, // Ensure email is included in the result
+        },
+      });
+
+      // Send emails to eligible users
+    //   for (const tenant of eligibleTenants) {
+    //     if (tenant.email) {
+    //       await this.mailService.sendUserMailSubscription(
+    //         tenant.email,
+    //         'You need to subscribe.',
+    //       );
+    //       this.logger.log(`Email sent to: ${tenant.email}`);
+    //     }
+    //   }
+    } catch (error) {
+      this.logger.error('Error updating payment eligibility', error);
+    }
+  }
+}
