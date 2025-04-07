2c9c4b5fb6f6e06ca28e9ab8616d710df2f11d3a
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-04-07 18:54:57 +0530

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+  //const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true, logger: ['error', 'warn', 'log', 'debug'] });
+  app.setGlobalPrefix(process.env.API_PREFIX, {
+    exclude: ['/'],
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
+  //cors configuration
+  app.enableCors();
+  //await app.listen(process.env.PORT ?? 4000);
+  await app.listen(configService.get('app.port') || 4000, () => {
+    console.log(`Server running at port: ${configService.get('app.port')}`);
+  });
 }
 bootstrap();
